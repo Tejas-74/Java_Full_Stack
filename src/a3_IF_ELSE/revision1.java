81db0f6450f6cbaa1b261035ff15c74725c22482
Tejas Jadhav <tejasnjadhav76@gmail.com>
2026-01-09 23:31:43 +0530

    Java

---
+++ src/a3_IF_ELSE/revision1.java
@@ -0,0 +1,22 @@
+package a3_IF_ELSE;
+import java.util.Scanner;
+public class revision1 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter SP: ");
+        int sp = sc.nextInt();
+
+        System.out.println("Enter CP: ");
+        int cp = sc.nextInt();
+
+        if(sp > cp){
+            System.out.println("You are in Profit");
+            System.out.println("Your Profit is: "+(sp-cp)); // sp-sp = profit
+        }else if(sp < cp){
+            System.out.println("You are in Loss");
+            System.out.println("Your Loss is: "+(cp-sp)); // cp-sp = loss
+        }
+    }
+}
+//push
